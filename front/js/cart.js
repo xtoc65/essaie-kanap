113161b8a3d1d0cfc1f9413c1f01339715f82481
Flora Paul <flora.p3108@gmail.com>
2022-10-09 15:42:04 +0200

    passer commande

--- front/js/cart.js
+++ front/js/cart.js
@@ -43,6 +43,7 @@ async function afficher(panier)
     calculerTotal(); 
 }
 
+//modifier la quantité d'un produit du panier
 function modifierQuantite()
 {
     document.querySelectorAll('.itemQuantity').forEach(
@@ -59,13 +60,12 @@ function modifierQuantite()
                     localStorage.setItem("panier", JSON.stringify(panier));
                     calculerTotal();
                 }        
-
             })
         }
     )
-    
 }
 
+//calculer un prix et la quantité d'un produit du panier
 function calculerTotal(){
     let totalQuantite = 0;  
     let totalPrix = 0;         
@@ -84,8 +84,8 @@ function calculerTotal(){
 }
 
 
-
-function supprimerProduit(){ //retirer un produit du panier
+//supprimer un produit du panier
+function supprimerProduit(){ 
     document.querySelectorAll('.deleteItem').forEach(
         suppirmerElement =>{
             suppirmerElement.addEventListener("click", ()=>
@@ -106,3 +106,139 @@ function supprimerProduit(){ //retirer un produit du panier
     
 }
 
+// ******** Formulaire de validation de la commande & envois de celle-ci à l'API ********
+
+// Récupération de chaque élément du form dans le DOM
+let prenomEl = document.getElementById("firstName");
+let nomEl = document.getElementById("lastName");
+let adresseEl = document.getElementById("address");
+let villeEl = document.getElementById("city");
+let emailEl = document.getElementById("email");
+
+let prenom;
+let nom;
+let adresse;
+let ville;
+let email;
+
+let prenomRegExp = new RegExp(
+    '^[a-zA-Z]+$', 'g' //Le début du texte commence par des caractéres qu'on peux ecrire plusieur fois.
+    );                 // le $ designe la fin de l'expresion réfgulière. g est le marqueur pour dire global;
+let nomRegExp = new RegExp(
+    '^[a-zA-Z]+$', 'g' //Le début du texte commence par des caractéres qu'on peux ecrire plusieur fois.
+);                 // le $ designe la fin de l'expresion réfgulière. g est le marqueur pour dire global
+let adresseRegExp = new RegExp(
+    '^[a-zA-Z0-9-\s]+$', 'g' //Le début du texte commence par des caractéres qu'on peux ecrire plusieur fois.
+);                       // le $ designe la fin de l'expresion réfgulière. g est le marqueur pour dire global
+let villeRegExp = new RegExp(
+    '^[a-zA-Z-]+$', 'g' //Le début du texte commence par des caractéres qu'on peux ecrire plusieur fois.
+);                            // le $ designe la fin de l'expresion réfgulière. g est le marqueur pour dire global
+let emailRegExp = new RegExp(
+    '^[a-zA-Z0-9\.-_]+[@]{1}[a-zA-Z0-9\.-_]+[\.]{1}[a-z]{2,3}$', 'g' //Le début du texte commence par des caractéres qu'on peux ecrire plusieur fois.On doit retrouvé le @ 1 seule fois
+);                                                                // le $ designe la fin de l'expresion réfgulière. g est le marqueur pour dire global
+
+prenomEl.addEventListener('change', function(){
+    validationPrenom(this);
+});
+const validationPrenom = function (prenomEl){
+    if(prenomRegExp.test(prenomEl.value)){
+        document.getElementById("firstNameErrorMsg").textContent="";
+        prenom = prenomEl.value;
+    }else{
+        document.getElementById("firstNameErrorMsg").textContent="votre prénom n'est pas valide";
+        prenom = null;
+    }
+};
+
+nomEl.addEventListener('change', function(){
+    validationNom(this);
+});
+const validationNom = function (nomEl){
+    if(nomRegExp.test(nomEl.value)){
+        document.getElementById("lastNameErrorMsg").textContent="";
+        nom = nomEl.value;
+    }else{
+        document.getElementById("lastNameErrorMsg").textContent="votre nom n'est pas valide";
+        nom = null;
+    }
+};
+
+adresseEl.addEventListener('change', function(){
+    validationAdresse(this);
+});
+const validationAdresse = function (adresseEl){
+    if(adresseRegExp.test(adresseEl.value)){
+       document.getElementById("addressErrorMsg").textContent="";
+       adresse = adresseEl.value;
+    }else{
+        document.getElementById("addressErrorMsg").textContent="votre adresse n'est pas valide";
+        adresse = null;
+    }
+};
+
+villeEl.addEventListener('change', function(){
+    validationVille(this);
+});
+const validationVille = function (villeEl){
+    if(villeRegExp.test(villeEl.value)){
+        document.getElementById("cityErrorMsg").textContent="";
+        ville = villeEl.value;
+    }else{
+        document.getElementById("cityErrorMsg").textContent="votre ville n'est pas valide";
+        ville = null;
+    }
+};
+
+emailEl.addEventListener('change', function(){
+    validationEmail(this);
+});
+const validationEmail = function (emailEl){
+    if(emailRegExp.test(emailEl.value)){
+        document.getElementById("emailErrorMsg").textContent="";
+        email = emailEl.value;
+    }else{
+        document.getElementById("emailErrorMsg").textContent="votre email n'est pas valide";
+        email = null;
+    }
+};
+
+function commander(event){
+    event.preventDefault();
+    if(prenom == null || nom == null || adresse == null || ville == null || email == null){
+        alert("veulliez remplir le fomulaire correctement");
+    }
+    if(!panier.length){
+        alert("veuillez remplir le panier");
+    }
+    contact = {
+        firstName : prenom,
+        lastName : nom,
+        address : adresse,
+        city : ville,
+        email : email
+    };
+    products = [];
+    panier.forEach(produit => products.push(produit.id));
+    fetch("http://localhost:3000/api/products/order", {
+	    method: 'POST',
+	    headers: { 
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json' 
+        },
+	    body: JSON.stringify({contact:contact, products:products})
+    }).then(function(res){
+        if(res.ok){
+            return res.json();
+        }else{
+            alert("la commande n'est pas valide");
+        };
+    })
+    .then(res => {
+        console.log(res);
+        if(res.orderId){
+            alert("commande validé on va te redirigé vers la page de confirmation je vais faire ca dans la semaine");
+        }
+    }
+    );    
+}
+document.querySelector(".cart__order__form").addEventListener("submit", commander);
